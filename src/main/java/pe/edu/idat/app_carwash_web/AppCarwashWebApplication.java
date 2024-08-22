a1f20c2a6db9da7ba35b11684da2ca8a75ef8cfd
julces180 <jul972009@gmail.com>
2024-08-21 20:50:39 -0500

    cors configurado

--- src/main/java/pe/edu/idat/app_carwash_web/AppCarwashWebApplication.java
+++ src/main/java/pe/edu/idat/app_carwash_web/AppCarwashWebApplication.java
@@ -2,12 +2,26 @@ package pe.edu.idat.app_carwash_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppCarwashWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppCarwashWebApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer configGlobalCors(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
